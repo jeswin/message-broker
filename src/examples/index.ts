4529f0bb9f17f8e480e9d1ca5c11004a0d895dd9
Jeswin Kumar <jeswinpk@agilehead.com>
2024-11-26 12:47:34 +0530

    WIP

---
+++ src/examples/index.ts
@@ -0,0 +1,17 @@
+import { BrokerType, createMessageBroker } from "../messageBroker.js";
+import { createMessageBrokerClient } from "../messageBrokerClient.js";
+
+function getBroker() {
+  return createMessageBroker(() => {}).attachHandler(
+    "adder",
+    async (params: { a: number; b: number }) => params.a + params.b
+  );
+}
+
+function getBrokerClient() {
+  return createMessageBrokerClient<BrokerType<typeof getBroker>>(() => {});
+}
+
+const client = getBrokerClient();
+
+const sum = await client.send("adder", { a: 10, b: 20 });
